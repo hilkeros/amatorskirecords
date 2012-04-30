cce0421b0c509b4116e618b9a63d3c275579f62a
Hilke Ros <hilke@mmmotion.com>
2012-04-30 18:50:30 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Recordstore::Application.config.secret_token = '34c7af85144713f75cab4284a60e7de35529c74f6a98abf64a541f9bc5b84b8045be989c5ccffb9b7aab1153bc54c012e8d66b79b34263d5d2450d0e8e4e7fe9'
